284cefb85534a8c63e3d4084465cc98f71a6c8b1
Szymon Ludwiniak <259215@student.pwr.edu.pl>
2024-06-08 17:16:17 +0200

    change target

---
+++ include/drivers/wifi_uart/interface/i_wifi_uart.hpp
@@ -0,0 +1,10 @@
+#ifndef I_WIFI_UART_HPP
+#define I_WIFI_UART_HPP
+
+class IWifiUart {
+   public:
+    IWifiUart();
+    virtual ~IWifiUart() = default;
+};
+
+#endif
--- tank-script.sh
+++ tank-script.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 readonly PROJECT_ROOT=$(pwd)
-readonly STM_BOARD="stm32f429i_disc1"
+readonly M4_BOARD="stm32h747i_disco_m4"
+readonly M7_BOARD="stm32h747i_disco_m7"
 readonly BUILD_PATH="${PROJECT_ROOT}/build"
-readonly BUILD_PATH_TEST="${PROJECT_ROOT}/build_test"
+readonly RUNNER="openocd"
 
 clean() {
     rm -rf $BUILD_PATH/*
-    rm -rf $BUILD_PATH_TEST/*
 }
 
-build_stm() {
-    mkdir -p $BUILD_PATH/$STM_BOARD
-    west build -b $STM_BOARD $PROJECT_ROOT --build-dir $BUILD_PATH/$STM_BOARD
+build_m4() {
+    mkdir -p $BUILD_PATH/$M4_BOARD
+    west build -b $M4_BOARD $PROJECT_ROOT --build-dir $BUILD_PATH/$M4_BOARD -D CHIP=m4
+}
+
+build_m7() {
+    mkdir -p $BUILD_PATH/$M7_BOARD
+    west build -b $M7_BOARD $PROJECT_ROOT --build-dir $BUILD_PATH/$M7_BOARD -D CHIP=m7
 }
 
 test() {
+    local -r build_test_dir="${PROJECT_ROOT}/build_test"
     
-    mkdir -p ${BUILD_PATH_TEST}
-    cd ${BUILD_PATH_TEST}
+    mkdir -p ${build_test_dir}
+    cd ${build_test_dir}
     cmake -DBUILD_TESTING=ON ${PROJECT_ROOT}
 
     # Run build tool if CMake generation finished successfully 
@@ -32,16 +38,24 @@ test() {
 
 }
 
-flash_stm() {
-    west flash -d $BUILD_PATH/$STM_BOARD
+flash_m4() {
+    west flash -d $BUILD_PATH/$M4_BOARD --runner $RUNNER
+}
+
+flash_m7() {
+    west flash -d $BUILD_PATH/$M7_BOARD --runner $RUNNER
 }
 
 help() {
-    local message="Supported commands\n"
-    message+="-b, --build\tbuild target for stm32f429 disc1\n"
-    message+="-f, --flash\tflash stm32f429 disc1\n"
-    message+="-t, --test\t\ttest project\n"
-    message+="-c, --clean\t\tclean the current build\n"
+    local message="**SUPPORTED COMMANDS**\n\n"
+    message+="-b,\t--build\t\tbuild both targets\n"
+    message+="-bm4,\t--build_m4\tbuild target for cortex-m4\n"
+    message+="-bm7,\t--build_m7\tbuild target for cortex-m7\n"
+    message+="-f,\t--flash\t\tflash both targets\n"
+    message+="-fm4,\t--flash_m4\tflash cortex-m4\n"
+    message+="-fm7,\t--flash_m7\tflash cortex-m7\n"
+    message+="-t,\t--test\t\ttest project\n"
+    message+="-c,\t--clean\t\tclean the current build\n"
 
     echo -e ${message}
 }
@@ -50,13 +64,27 @@ main() {
     local -r command=$1
     case ${command} in
         -b|--build)
-            build_stm
+            build_m4
+            build_m7
+        ;;
+        -bm4|--build_m4)
+            build_m4
+        ;;
+        -bm7|--build_m7)
+            build_m7
         ;;
         -t|--test)
             test
         ;;
         -f|--flash)
-            flash_stm
+            flash_m4
+            flash_m7
+        ;;
+        -fm4|--flash_m4)
+            flash_m4
+        ;;
+        -fm7|--flash_m7)
+            flash_m7
         ;;
         -c|--clean)
             clean
